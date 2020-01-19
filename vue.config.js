3e7c0ed3c85d55bfef88728f9f00b11060ef0063
Janko <shuaijinchao@gmail.com>
2020-01-19 17:47:08 +0800

    change: update default admin server address.

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,10 @@ module.exports = {
     devServer: {
         proxy: {
             "/apioak": {
-                target: "http://49.232.143.79:10080",
+                target: "http://127.0.0.1:10080",
                 changeOrigin: true
             }
         }
     },
-
     lintOnSave: false
 }
