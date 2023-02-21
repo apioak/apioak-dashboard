3bba30dc611d8d12b0513e07c0a0056b0e0b11ba
blust <blust_h@sina.com>
2023-02-21 22:26:24 +0800

    feat: update mock plugin schema content

--- src/schema/plugins/mock.js
+++ src/schema/plugins/mock.js
@@ -1,17 +1,29 @@
 const nameMax = { max: 30, message: '名称最长30个字符！' }
 
-// const rateLenBetween = { type: 'number', min: 1, max: 100000, message: '当前值必须在 1-100000 之间！' }
-//
-// const burstLenBetween = { type: 'number', min: 0, max: 5000, message: '当前值必须在 0-5000 之间！' }
+const responseType = {
+  type: "string",
+  required: true,
+  pattern: /^[/+-.a-zA-Z1-9]+$/,
+  message: '当前值仅包含字母、数字以及+-./符号！'
+}
 
-const headerFormat = {
-  pattern: /^[A-Za-z1-9_-]+$/,
-  message: '当前值仅包含字母、数字、划线'
+const httpCode = {
+  type: 'number',
+  required: true,
+  min: 100,
+  max: 999,
+  message: '当前值必须在 100-999 之间！'
+}
+
+const httpBody = {
+  type: "string",
+  required: true,
+  message: "请输入http_body"
 }
 
 export const schemaPluginMock = {
   name: [nameMax],
-  response_type:[ {type: "string", required: true, message: "请输入response_type"}],
-  http_code:[{ type: "number", required: true, message: "请输入http_code" }],
-  http_body:[{ type: "string", required: true, message: "请输入http_body" }],
+  response_type:[responseType],
+  http_code:[httpCode],
+  http_body:[httpBody],
 }
