47385029e8ea072f17ffb9a8989969095481ec11
zanehy <zanehy@foxmail.com>
2023-02-27 11:49:16 +0800

    feat: upgrade the verification rules of the route

--- src/schema/router.js
+++ src/schema/router.js
@@ -4,9 +4,14 @@ const nameFormat = {
 }
 const nameMax = { max: 30, message: '名称最长30个字符!' }
 
+const pathFormat = {
+  pattern: /^\/[(A-Za-z)\d_\-./*=]+$/,
+  message: '路径必须以“/”开头，仅支持字母、数字、 / _ - . * ='
+}
+
 export const schemaRouter = {
   name: [nameFormat, nameMax],
   service_res_id: [{ required: true, message: '所属服务不能为空' }],
-  router_path: [{ required: true, message: '路由路径不能为空' }],
+  router_path: [{ required: true, message: '路由路径不能为空' }, pathFormat],
   request_methods: [{ required: true, message: '请求方法不能为空' }]
 }
